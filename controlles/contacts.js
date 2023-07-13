86adc119fef5bc1e212a8b4ca02219038f095e10
Iryna Lazareva <il337733@gmail.com>
2023-07-13 17:08:11 +0300

    change

---
+++ controlles/contacts.js
@@ -0,0 +1,98 @@
+const {Contact} = require("../models/contacts");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const listContacts = async (req, res) => {
+    const results = await Contact.find()
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        contacts: results,
+      },
+    })
+}
+
+const getContactById  = async (req, res) => {
+    const { id } = req.params;
+    
+    const result = await Contact.findById(id);
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+    res.json(result);
+}
+
+const addContact  = async (req, res) => {
+    const { name, email, phone } = req.body;
+    const { error } = req.body;
+    if (error) {
+      throw HttpError(400, `Missing required name field`)
+    }
+    const result = await Contact.create({ name, email, phone})
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { contact: result },
+    })
+}
+
+const updateContact  = async (req, res) => {
+    const { id } = req.params
+  const { name, email, phone } = req.body
+    const { error } = req.body;
+    if (error) {
+      throw HttpError(400, `Missing required name field`)
+    }
+    const result = await Contact.findByIdAndUpdate({ _id: id }, { name, email, phone }, { new: true })
+    if (result) {
+      res.json({
+        status: 'success',
+        code: 200,
+        data: { contact: result },
+      })
+    } else {
+      throw HttpError(404, `Not found contact id: ${id}`)
+    }
+}
+
+const updateFavorite = async (req, res) => {
+    const { id } = req.params
+    const { error } = req.body;
+  if (error) {
+      throw HttpError(400, 'Missing required name field')
+    }
+    const result = await Contact.findByIdAndUpdate({ _id: id }, req.body, { new: true })
+    if (result) {
+      res.json({
+        status: 'success',
+        code: 200,
+        data: { contact: result },
+      })
+    } else {
+      throw HttpError(404, `Not found contact id: ${id}`)
+    }
+}
+
+const removeContact  = async (req, res) => {
+    const { id } = req.params
+    const result = await Contact.findByIdAndRemove({ _id: id })
+    if (result) {
+      res.json({
+        status: 'success',
+        code: 200,
+        data: { contact: result },
+      })
+    } else {
+      throw HttpError(404, `Not found contact id: ${id}`)
+    }
+}
+
+module.exports = {
+    listContacts: ctrlWrapper(listContacts),
+    getContactById: ctrlWrapper(getContactById),
+    addContact: ctrlWrapper(addContact),
+    updateContact: ctrlWrapper(updateContact),
+    updateFavorite: ctrlWrapper(updateFavorite),
+    removeContact: ctrlWrapper(removeContact),
+}
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,65 @@
-// const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const {Schema, model} = require("mongoose");
+const Joi = require("joi");
 
-const getContactById = async (contactId) => {}
+// const {handleMongooseError} = require("../helpers");
 
-const removeContact = async (contactId) => {}
+const contactSchema=new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  }, {versionKey: false, timestamps: true}
+)
+// contactSchema.post("save", handleMongooseError);
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 
-const addContact = async (body) => {}
+});
 
-const updateContact = async (contactId, body) => {}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+})
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+}
+
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
+  schemas,
 }
+
+// const listContacts = async () => {}
+
+// const getContactById = async (contactId) => {}
+
+// const removeContact = async (contactId) => {}
+
+// const addContact = async (body) => {}
+
+// const updateContact = async (contactId, body) => {}
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
